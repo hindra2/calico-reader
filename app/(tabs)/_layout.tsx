de2924be1a0980b53247dc7cfa3eb0b14a4676f5
hindra2 <harold.indra@gmail.com>
2025-06-13 11:00:10 +0700

    added tab bar styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,25 @@
 import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Pressable, View } from 'react-native';
 import { useColorScheme } from 'nativewind';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-
 import Colors from '@/constants/Colors';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string }) {
-    return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+function TabBarIcon(props: {
+    name: React.ComponentProps<typeof FontAwesome>['name'];
+    color: string;
+    focused: boolean;
+}) {
+    return (
+        <View
+            className={`w-20 h-12 items-center justify-center rounded-full overflow-hidden`}
+            style={{
+                backgroundColor: props.focused ? `${props.color}50` : 'transparent',
+            }}
+        >
+            <FontAwesome size={28} {...props} />
+        </View>
+    );
 }
 
 export default function TabLayout() {
@@ -18,19 +30,45 @@ export default function TabLayout() {
             screenOptions={{
                 tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
                 headerShown: false,
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    backgroundColor: 'transparent',
+                    borderTopWidth: 0,
+                    height: 70,
+                    justifyContent: 'center',
+                    paddingTop: 19,
+                },
+                tabBarBackground: () => (
+                    <View
+                        className="flex-1"
+                        style={{ backgroundColor: Colors[colorScheme ?? 'light'].tabBarBackground }}
+                    />
+                ),
+                tabBarButton: props => {
+                    return (
+                        <Pressable
+                            onPress={props.onPress}
+                            onLongPress={props.onLongPress}
+                            android_ripple={{ color: 'transparent' }}
+                            className="items-center justify-center"
+                        >
+                            {props.children}
+                        </Pressable>
+                    );
+                },
             }}
         >
             <Tabs.Screen
                 name="index"
                 options={{
-                    tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+                    tabBarIcon: ({ color, focused }) => <TabBarIcon name="home" color={color} focused={focused} />,
                     title: 'Home',
                 }}
             />
             <Tabs.Screen
                 name="settings"
                 options={{
-                    tabBarIcon: ({ color }) => <TabBarIcon name="cog" color={color} />,
+                    tabBarIcon: ({ color, focused }) => <TabBarIcon name="cog" color={color} focused={focused} />,
                     title: 'Settings',
                 }}
             />
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,21 @@
 const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorDark = '#ffffff';
 
 export default {
-  dark: {
-    background: '#000',
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-    text: '#fff',
-    tint: tintColorDark,
-  },
-  light: {
-    background: '#fff',
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-    text: '#000',
-    tint: tintColorLight,
-  },
+    dark: {
+        background: '#000',
+        tabIconDefault: '#ccc',
+        tabBarBackground: '#222',
+        tabIconSelected: tintColorDark,
+        text: '#fff',
+        tint: tintColorDark,
+    },
+    light: {
+        background: '#fff',
+        tabIconDefault: '#ccc',
+        tabBarBackground: '#eee',
+        tabIconSelected: tintColorLight,
+        text: '#000',
+        tint: tintColorLight,
+    },
 };
