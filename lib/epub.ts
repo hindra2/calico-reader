4380247649b97d060e706901c34cd13e8b4fd6bd
hindra2 <harold.indra@gmail.com>
2025-06-25 16:40:26 +0700

    removed console logs

--- lib/epub.ts
+++ lib/epub.ts
@@ -3,7 +3,6 @@ import { Metadata } from '@/modules/CalicoParser';
 
 const loadEpub = async (uri: string): Promise<Metadata> => {
     const result = await CalicoParser.parseEpub(uri);
-    console.log('metadata:', result);
     return result;
 };
 
